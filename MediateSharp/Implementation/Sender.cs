b8ac6c0e334bd4f737a7b956a5fc1ea50b65738c
Nicole Turner <astartechie@gmail.com>
2025-04-07 21:11:11 +0100

    Implemented Sender with Tests

---
+++ MediateSharp/Implementation/Sender.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MediateSharp.Implementation;
+
+internal class Sender(IServiceProvider serviceProvider) : ISender
+{
+    public Task SendAsync<TRequest>(TRequest request) where TRequest : IRequest
+    {
+        var requestHandler = serviceProvider.GetService<IRequestHandler<TRequest>>();
+        if (requestHandler is null) throw new UnknownRequestException();
+        return requestHandler.HandleAsync(request);
+    }
+
+    public Task<TResponse> SendAsync<TRequest, TResponse>(TRequest request) where TRequest : IRequest<TResponse>
+    {
+        var requestHandler = serviceProvider.GetService<IRequestHandler<TRequest, TResponse>>();
+        if (requestHandler is null) throw new UnknownRequestException();
+        return requestHandler.HandleAsync(request);
+    }
+}
