2af06c1b881091575bd4764c249a67b336d27cea
Nicole Turner <astartechie@gmail.com>
2025-04-07 21:11:25 +0100

    Implemented Dependency Injection

---
+++ MediateSharp/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MediateSharp.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediateSharp.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMediateSharp(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<ISender, Sender>();
+
+        return serviceCollection;
+    }
+}
